d8fe4c0f9bb7f01fb6a182bdd900cc582443345d
roxabhi99 <113662780+roxabhi99@users.noreply.github.com>
2023-04-18 12:28:40 +0530

    Update jenkins-setup.sh

--- jenkins-setup.sh
+++ jenkins-setup.sh
@@ -4,7 +4,7 @@
 #!/bin/bash
 sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo amazon-linux-extras install epel -y
 sudo yum install java-11* -y
 sudo yum install jenkins -y
